db7e5d93253bfafb94f461f523e42636696f0f44
Badar Wildani <wildanie12@gmail.com>
2022-05-14 10:47:22 +0700

    add: javascript entrypoint development

---
+++ src/index.js
@@ -0,0 +1,15 @@
+if (localStorage.getItem('theme') == 'dark') setDarkMode()
+
+function setDarkMode() {
+    let emoticon = ''
+    let isDark = document.body.classList.toggle('dark')
+
+    if (isDark) {
+        emoticon = '<i class="bi bi-brightness-high"></i>'
+        localStorage.setItem('theme', 'dark')
+    } else {
+        emoticon = '<i class="bi bi-moon"></i>'
+        localStorage.removeItem('theme')
+    }
+    document.getElementById('darkBtn').innerHTML = emoticon
+}
