bb96ea78b7b5bf5af18b6f1842e0d3a33a6de143
Badar Wildani <wildanie12@gmail.com>
2022-05-20 11:28:11 +0700

    fix: darkmode refactoring and restyling

--- src/index.js
+++ src/index.js
@@ -10,10 +10,57 @@ import {
 } from './sebaran'
 import { refreshNewsPage } from './news_page'
 import { initMap } from './maps'
+import { initChart, refreshDailyStats, refreshProvinceStats } from './chart'
+import { onMount, refreshRujukanRS } from './rs_rujukan'
+
+let sebaranCharts = {}
 
 // Dark Mode
-if (localStorage.getItem('theme') == 'dark') setDarkModeOn()
-export const setDarkMode = setDarkModeOn
+let isDark = false
+export const setDarkMode = () => {
+    let emoticon = ''
+    isDark = document.body.classList.toggle('dark')
+
+    if (isDark) {
+        emoticon = '<i class="bi bi-brightness-high"></i>'
+        localStorage.setItem('theme', 'dark')
+    } else {
+        emoticon = '<i class="bi bi-moon"></i>'
+        localStorage.removeItem('theme')
+    }
+    document.getElementById('darkBtn').innerHTML = emoticon
+
+    if (Object.keys(sebaranCharts).length !== 0) {
+        if (isDark) {
+            sebaranCharts.positifday.options.scales.y.ticks.color = '#E8A421'
+            sebaranCharts.positifday.options.scales.x.ticks.color = '#E8A421'
+            sebaranCharts.negatifday.options.scales.y.ticks.color = '#E8A421'
+            sebaranCharts.negatifday.options.scales.x.ticks.color = '#E8A421'
+            sebaranCharts.deathday.options.scales.y.ticks.color = '#E8A421'
+            sebaranCharts.deathday.options.scales.x.ticks.color = '#E8A421'
+            sebaranCharts.mixeddata.options.scales.y.ticks.color = '#E8A421'
+            sebaranCharts.mixeddata.options.scales.x.ticks.color = '#E8A421'
+            sebaranCharts.provdata.options.scales.y.ticks.color = '#E8A421'
+            sebaranCharts.provdata.options.scales.x.ticks.color = '#E8A421'
+        } else {
+            sebaranCharts.positifday.options.scales.y.ticks.color = '#E8A421'
+            sebaranCharts.positifday.options.scales.x.ticks.color = '#E8A421'
+            sebaranCharts.negatifday.options.scales.y.ticks.color = '#8D94A0'
+            sebaranCharts.negatifday.options.scales.x.ticks.color = '#8D94A0'
+            sebaranCharts.deathday.options.scales.y.ticks.color = '#8D94A0'
+            sebaranCharts.deathday.options.scales.x.ticks.color = '#8D94A0'
+            sebaranCharts.mixeddata.options.scales.y.ticks.color = '#8D94A0'
+            sebaranCharts.mixeddata.options.scales.x.ticks.color = '#8D94A0'
+            sebaranCharts.provdata.options.scales.y.ticks.color = '#8D94A0'
+            sebaranCharts.provdata.options.scales.x.ticks.color = '#8D94A0'
+        }
+        sebaranCharts.positifday.update()
+        sebaranCharts.negatifday.update()
+        sebaranCharts.deathday.update()
+        sebaranCharts.mixeddata.update()
+    }
+}
+if (localStorage.getItem('theme') == 'dark') setDarkMode()
 
 export const homepageScript = () => {
     refreshNewsCarousel()
@@ -71,4 +118,8 @@ export const sebaranScript = () => {
         }
     })
 }
+
+export const rsRujukanScript = () => {
+    onMount()
+    refreshRujukanRS()
 }
