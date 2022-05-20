e2235a4200a75c3809503a688589c8b9538e7abb
Badar Wildani <wildanie12@gmail.com>
2022-05-20 20:15:21 +0700

    feat: added pagination query parameter news endpoint

--- api/controllers/news_controller.js
+++ api/controllers/news_controller.js
@@ -5,17 +5,43 @@ const parser = new Parser({
         'User-Agent': 'application',
     },
 })
+let covid19News = []
 
 module.exports = {
     findAllCovid19: async (req, res) => {
         try {
-            feed = await parser.parseURL(
-                'https://www.voaindonesia.com/api/zt-yqreivvqr'
-            )
+            let page = parseInt(req.query.page)
+            let limit = parseInt(req.query.limit)
+            let offset = parseInt(req.query.offset)
+            if (!page) page = 1
+            if (!limit) limit = 50
+            if (!offset) offset = limit * (page - 1)
+
+            if (covid19News.length <= 0) {
+                const data = await parser.parseURL(
+                    'https://www.voaindonesia.com/api/zt-yqreivvqr'
+                )
+                covid19News = data.items
+                setTimeout(() => {
+                    covid19News = []
+                }, 1000 * 60 * 30)
+            }
+
+            let totalPages = parseInt(covid19News.length / limit)
+            if (covid19News.length % limit > 0) totalPages++
+
+            let covid19NewsOps = covid19News.slice()
             return res.json({
                 status: 'OK',
                 code: 200,
-                data: feed,
+                data: covid19NewsOps.splice(offset, limit),
+                pagination: {
+                    page,
+                    limit,
+                    offset,
+                    total_pages: totalPages,
+                    total_count: covid19News.length,
+                },
             })
         } catch (error) {
             console.log(error)
