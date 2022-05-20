75f60c948064caa6537d9e5e5766fa40b1ab2987
Badar Wildani <wildanie12@gmail.com>
2022-05-20 19:38:58 +0700

    add: province data response endpoint

--- api/server.js
+++ api/server.js
@@ -4,7 +4,9 @@ const app = express()
 const port = process.env.SERVER_PORT || 5000
 const cors = require('cors')
 
+app.use(express.static('api/public'))
 app.use(cors())
+
 app.use('/api/news', require('./routes/news_route'))
 app.use('/api/stats', require('./routes/stats_route'))
 app.use('/api/references', require('./routes/reference_route'))
