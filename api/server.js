35ec348e67ead4d5310b82c3ac4a0967a4e109f9
Badar Wildani <wildanie12@gmail.com>
2022-05-20 11:31:33 +0700

    feat: add rujukan proxy server implementation

--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.SERVER_PORT || 5000
 const cors = require('cors')
 
 app.use(cors())
 app.use('/api/news', require('./routes/news_route'))
 app.use('/api/stats', require('./routes/stats_route'))
+app.use('/api/references', require('./routes/reference_route'))
 app.use('/api/tests', require('./routes/test_route'))
 
 app.listen(port, () => {
