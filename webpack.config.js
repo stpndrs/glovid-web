e97cb0c42e9dbc9b1f7b75aa9500aa82df92bd1a
Badar Wildanie <wildanie12@gmail.com>
2022-05-21 12:00:16 +0700

    perf: optimize resource build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'script.js',
         path: path.resolve(__dirname, 'dist/js'),
         library: 'glovid',
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
     },
     plugins: [new Dotenv()],
 }
